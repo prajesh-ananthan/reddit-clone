2a7ab896873c10c190f54e4ba69c5df690e0abda
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2019-01-21 23:19:55 +0800

    Initial commit

---
+++ src/main/java/io/reddit/domain/BaseDomain.java
@@ -0,0 +1,53 @@
+package io.reddit.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Created by Prajesh on 21/1/2019.
+ */
+
+@MappedSuperclass
+public class BaseDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date createdDate;
+    private Date modifiedDate;
+
+    @PreUpdate
+    @PrePersist
+    public void updateTimestamps() {
+        modifiedDate = new Date();
+        if (Objects.nonNull(createdDate)) {
+            createdDate = new Date();
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
